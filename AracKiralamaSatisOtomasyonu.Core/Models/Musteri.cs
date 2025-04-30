a60c717781322777686c97e1ccbadc46076c1e57
gokayoz <gokay.oz@outlook.com.tr>
2025-04-30 22:43:29 +0300

    Adds Musteri class and its properties

---
+++ AracKiralamaSatisOtomasyonu.Core/Models/Musteri.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AracKiralamaSatisOtomasyonu.Core.Models
+{
+    public class Musteri
+    {
+        public int Id { get; set; }
+        public string AdSoyad { get; set; }
+        public string Telefon { get; set; }
+        public string Email { get; set; }
+        public string Adres { get; set; }
+        public List<Kiralama> Kiralamalar { get; set; }
+        public List<Satis> Satislar { get; set; }
+    }
+}
