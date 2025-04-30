645b508315c9ab83ec7724e66b6d4be2ec77bb23
gokayoz <gokay.oz@outlook.com.tr>
2025-04-30 22:43:11 +0300

    Adds Kiralama class and its properties

---
+++ AracKiralamaSatisOtomasyonu.Core/Models/Kiralama.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AracKiralamaSatisOtomasyonu.Core.Models
+{
+    public class Kiralama
+    {
+        public int Id { get; set; }
+        public int AracId { get; set; }
+        public Arac Arac { get; set; }
+        public int MusteriId { get; set; }
+        public Musteri Musteri { get; set; }
+
+        public DateTime BaslangicTarihi { get; set; }
+        public DateTime BitisTarihi { get; set; }
+        public decimal GunlukUcret { get; set; }
+        public decimal ToplamTutar => (decimal)(BitisTarihi - BaslangicTarihi).TotalDays * GunlukUcret;
+    }
+}
