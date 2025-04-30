b153ee668080f46b51bd3a49ce96d5ec7d252da2
gokayoz <gokay.oz@outlook.com.tr>
2025-04-30 22:42:52 +0300

    Adds Araba class and its properties

---
+++ AracKiralamaSatisOtomasyonu.Core/Models/Araba.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AracKiralamaSatisOtomasyonu.Core.Models
+{
+    public class Arac
+    {
+        public int Id { get; set; }
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public int Yil { get; set; }
+        public int Kilometre { get; set; }
+        public string YakitTuru { get; set; }
+        public string SanzimanTipi { get; set; }
+        public string Renk { get; set; }
+        public string Durum { get; set; } 
+        public string MevcutDurum { get; set; }
+        public int KategoriId { get; set; }
+        public Kategori Kategori { get; set; }
+        public List<Kiralama> Kiralamalar { get; set; }
+        public List<Satis> Satislar { get; set; }
+    }
+}
