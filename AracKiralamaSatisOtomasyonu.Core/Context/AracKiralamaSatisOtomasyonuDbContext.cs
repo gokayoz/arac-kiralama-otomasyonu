ca8331f96cac614d1630a4b3e680ea5133a39a94
gokayoz <gokay.oz@outlook.com.tr>
2025-05-01 17:28:06 +0300

    Context folder, modelBuilder and adds DbSets. Makes configuration settings for the SQL connection

---
+++ AracKiralamaSatisOtomasyonu.Core/Context/AracKiralamaSatisOtomasyonuDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AracKiralamaSatisOtomasyonu.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AracKiralamaSatisOtomasyonu.Core.Context
+{
+    public class AracKiralamaSatisOtomasyonuDbContext : DbContext
+    {
+        public DbSet<Kategori> Kategori { get; set; }
+        public DbSet<Musteri> Musteri { get; set; }
+        public DbSet<Arac> Arac { get; set; }
+        public DbSet<Kiralama> Kiralama { get; set; }
+        public DbSet<Satis> Satis { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DENIZ\\SQLEXPRESS;Database=AracKiralamaSatisOtomasyonuDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Kategori>().HasData(
+                new Kategori { Id = 1, KategoriAdi = "Sedan" },
+                new Kategori { Id = 2, KategoriAdi = "SUV" },
+                new Kategori { Id = 3, KategoriAdi = "Hatchback" },
+                new Kategori { Id = 4, KategoriAdi = "Minivan" }
+                );
+        }
+    }
+}
